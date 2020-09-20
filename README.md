a50c3671fc3be5d05ad991c35b4abf5a27147d77
Leslie Villatoro <levill98@gmail.com>
2020-09-19 21:01:40 -0700

    finalized README, cleaned JS and added fixed issue with criteria confirms or cancels

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Password-Generator
- This random password generator will generate a password of your desired character amount within the range of 8-128 characters.
+ This random password generator will generate a password of your desired character amount within the range of 8-128 characters. User has option to pick what criteria they'd like for their passwords. If you'd like to you password to be 50 characters long and all upper case letters, feel free to do so!
 
 ## Feautres
 
 This web application consists of: 
- - Random Password generator 
+ - Prompt for desired character length
+ - Four confirms for: Special characters, Upper case letters, Lower case letters, and Numbers 
 
 
  ## Web application Image
@@ -15,9 +16,9 @@ This web application consists of:
 
 ## Built With
 
-* [JavaScript] 
-* [Git] - version control system to track changes to source code
-* [GitHub] - hosts repository that can be deployed to GitHub Pages
+* JavaScript 
+* Git - version control system to track changes to source code
+* GitHub - hosts repository that can be deployed to GitHub Pages
 
 
 ## Deployed Link
@@ -28,7 +29,6 @@ This web application consists of:
 ## Authors
 
 * **Leslie Villatoro** 
-
 - [Link to Github](https://github.com/leslievill)
 - [Link to LinkedIn](www.linkedin.com/in/leslie-villatoro-a3632a1a3)
 
--- script.js
+++ script.js
@@ -5,27 +5,23 @@ var generateBtn = document.querySelector("#generate");
 var lowCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upCase = ["A","B","C","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var specChar = ["~","`", "!","@","#","$","%","^","&","*","(",")","-","_","+","="]; 
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-var confirmLength = "";
-var confirmSpecChar = "";
-var confirmUpCase = "";
-var confirmLowCase = "";
-var confirmNum = "";
+var number = [0,1,2,3,4,5,6,7,8,9];
 
 //create prompt to select number of characters and Validate once entered
-function writePassword() {
+function generatePassword() {
 
   var confirmLength = prompt("How many characters would you like your password to be? Password must be between 8-128 characters.");
-  confirmLength=parseInt(confirmLength)
+  
+  confirmLength=parseInt(confirmLength);
 
 //validate length of characters 8-128
   if(confirmLength < 8 || confirmLength > 128 || isNaN(confirmLength)) {
+    
     return alert("Try entering a number between 8-128. Try again!");
+    
   } 
   
 //confirm for specChar, numbers, upper ,lower
-
   var confirmSpecChar = confirm("Click OK if you'd like to use special characters");
   var confirmNum = confirm("Click OK if you'd like to use numbers");    
   var confirmLowCase = confirm("Click OK if you'd like to use lowercase characters");
@@ -34,36 +30,65 @@ function writePassword() {
   
 // Loop if answer is outside the parameters 
   if(confirmUpCase === false && confirmLowCase === false && confirmSpecChar === false && confirmNum === false) {
-  return alert("You must choose at least one parameter");
+    
+    return alert("You must choose at least one parameter");
   
   } 
 //determine user choice for all four prompts one at a time
   var userInput = [];
-
-  if(lowCase) {
-  userInput = userInput.concat(lowCase);
-  }
-
-  if(upCase) {
-  userInput = userInput.concat(upCase);
-  }
-
-  if(number) {
-  userInput = userInput.concat(number);
-  }
-
-  if(specChar) {
-  userInput = userInput.concat(specChar);
-  }
-
-  var password = "";
-  for (var i = 0; i < confirmLength; i++) {
-    var ranNum = Math.floor(Math.random() * userInput.length);
-    password += userInput[ranNum] 
-  } console.log(password)
+  var confirmPass = [];
   
-// Write password to the #password input
+  if(confirmLowCase) {
+    userInput = userInput.concat(lowCase);
+    
+    var ranLow = Math.floor(Math.random() * lowCase.length);
+    
+    confirmPass += lowCase[ranLow];
+  }
+
+  if(confirmUpCase) {
+    userInput = userInput.concat(upCase);
+    
+    var ranUp = Math.floor(Math.random() * upCase.length);
+    
+    confirmPass += upCase[ranUp];
+  }
+
+  if(confirmNum) {
+    userInput = userInput.concat(number);
+    
+    var ranNumb = Math.floor(Math.random() * number.length);
+    
+    confirmPass += number[ranNumb];
+  }
+
+  if(confirmSpecChar) {
+    userInput = userInput.concat(specChar);
+   
+    var ranspecChar = Math.floor(Math.random() * specChar.length);
+    
+    confirmPass += specChar[ranspecChar];
+  }
+
+  var newPassword = " ";
+  
+  newPassword = newPassword.concat(confirmPass);
+  
+  for (var i = 0; i < confirmLength - confirmPass.length; i++) {
+    var ranNum = Math.floor(Math.random() * userInput.length);
+    
+    newPassword += userInput[ranNum]; 
+  } 
+  
+  return newPassword;
+
+}
+
+function writePassword() {
+  var password = generatePassword();
+  
   var passwordText = document.querySelector("#password");
+     
   passwordText.value = password;
 }
 
